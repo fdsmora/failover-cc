2edb43cee59559c03369a4b190c77313759c9f35
Fausto Salazar <fausto.ds.mora@gmail.com>
2019-06-16 12:49:38 -0500

    Run monitor and app as separate threads

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 import time
 from http.server import HTTPServer
-from server import Server
+from Monitor.monitor import Monitor 
+from Application.application import Application
+from multiprocessing import Process
 
 HOST_NAME = 'localhost'
-PORT_NUMBER = 8080
 
-
-if __name__ == '__main__':
-    with HTTPServer((HOST_NAME, PORT_NUMBER), Server) as httpd:
-        print(time.asctime(), 'Server UP - %s:%s' % (HOST_NAME, PORT_NUMBER))
+def start_monitor(port):
+    with HTTPServer((HOST_NAME, port), Monitor) as monitor:
+        print(time.asctime(), 'Server UP - %s:%s' % (HOST_NAME, port))
         try:    
-            httpd.serve_forever()
+            monitor.serve_forever()
         except KeyboardInterrupt:
             pass
+
+def start_instance(port):
+    with HTTPServer((HOST_NAME, port), Monitor) as app1:
+        print(time.asctime(), 'Server UP - %s:%s' % (HOST_NAME, port))
+        try:    
+            app1.serve_forever()
+        except KeyboardInterrupt:
+            pass
+
+if __name__ == '__main__':
+    monitor = Process(target=start_monitor, args=(8080,))
+    app1 = Process(target=start_instance, args=(8081,))
+    monitor.start()
+    app1.start()
+
